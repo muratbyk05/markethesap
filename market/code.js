69a3d32313bae668db259c6c08acc32227610ff3
muratbyk05 <95565340+muratbyk05@users.noreply.github.com>
2022-03-10 15:54:09 +0300

    Add files via upload

--- market/code.js
+++ market/code.js
@@ -5,10 +5,10 @@ function hesapla(){
     let sonuc=document.getElementById("sonuc")
     
     const para=100
-    let hesap = (Number(ürün1) + Number (ürün2) + Number (ürün3) - Number(para))
+    let hesap = ( Number(para)-Number(ürün1) + Number (ürün2) + Number (ürün3) )
 
     sonuc.innerHTML="Kalan paranız :"+hesap
-    if(hesap>=50){
+    if(hesap>=0){
         sonuc.innerHTML="Pacayı sıyırmadın: "+hesap
         document.body.style.backgroundColor="red"
     }
